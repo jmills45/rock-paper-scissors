b1f7949dfa4b42ef715e6e50da05d392f2b70d43
Joshua Mills <jmillsman33@gmail.com>
2023-06-06 10:32:46 -0500

    Add comments to javascript.js to explain code

--- javascript.js
+++ javascript.js
@@ -1,13 +1,16 @@
+// Declare global variables
 let gamesToPlay = 5;
 let playerScore = 0;
 let computerScore = 0;
 
+// Prompts the player for input and then converts all characters to lowercase
 function playerInput () {
     let rawInput = prompt("Please choose rock, paper, or scissors");
     cleanInput = rawInput.toLowerCase();
     return cleanInput;
 }
 
+// Randomly assigns the computer either rock, paper, or scissors
 function computerInput() {
     randomNumber = randomChoice(1, 3);
     if (randomNumber === 1) {
@@ -18,12 +21,16 @@ function computerInput() {
         return "scissors";
 }
 
+// Generates a random number between min and max
 function randomChoice(min, max) {
     return Math.floor(Math.random()* (max - min + 1)) + min;   
 }
 
+// Plays one round of rock, paper, scissors. Round is decided based on
+// The two arguments passed to the fuction.
 function playRound(player, computer) {
     switch (player) {
+        // All possible results if player chose rock
         case ("rock"):
             if (computer === "paper"){
                 computerScore = computerScore + 1;
@@ -34,7 +41,7 @@ function playRound(player, computer) {
             } else {
                 return "The computer choose rock. Its a Tie this round."
             }
-        
+        // All possible results if player chooses scissors
         case ("paper"):
             if (computer === "scissors"){
                 computerScore = computerScore + 1;
@@ -45,7 +52,7 @@ function playRound(player, computer) {
             } else {
                 return "The computer choose paper. Its a Tie this round."
             }
-
+        // All possible results if player chooses rock
         case ("scissors"):
             if (computer === "rock"){
                 computerScore = computerScore + 1;
@@ -58,7 +65,7 @@ function playRound(player, computer) {
             }
     }
 }
-
+// Decides the winner of the game by comparing playerFinalScore and computerFinalScore
 function decideWinner(playerFinalScore, computerFinalScore) {
     if (playerFinalScore > computerFinalScore){
         return "You Won!";
@@ -69,6 +76,8 @@ function decideWinner(playerFinalScore, computerFinalScore) {
     }
 }
 
+// Loop that plays the game. Player and Computer selection are freshed with each pass.
+// Points are totaled after each round and a winner is decided at the end of X rounds.
 for (i = gamesToPlay; i > 0; i--) {
 
     let playerSelection = playerInput();
@@ -77,10 +86,10 @@ for (i = gamesToPlay; i > 0; i--) {
     roundWinner = playRound(playerSelection, computerSelection)
 
     console.log(`The Player choose ${playerSelection}`)
-    console.log(`The Computer choose ${computerSelection}`)
     console.log(roundWinner);
 }
 
+// Report finall score of each player and then report the winner.
 console.log(playerScore);
 console.log(computerScore);
 
